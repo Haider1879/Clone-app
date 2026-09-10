fcc3b1d438a1ad1c1b0df37d8ad33cb373663fcf
Haider1879 <nasirijaz237@gmail.com>
2026-09-10 18:58:54 +0500

    Update

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import SignUp from './pages/SignUp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className="ticks">
+        <SignUp />
+      </div>
+
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,17 @@
+
+
+const SignUp: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
